e3c790c533648c7952df2d353256dbc30ef8066e
Aiuna San <158786728+pinklemon8@users.noreply.github.com>
2024-04-02 11:00:52 +0200

    added new file fixtures.py include fixtures

---
+++ lesson1/fixtures.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+import pytest
+
+url = 'https://www.saucedemo.com/'
+browser = webdriver.Chrome()
+
+
+# Фикстура для авторизации
+@pytest.fixture()
+def standard_auth():
+    browser.get(url)
+    browser.find_element('xpath', '//input[@data-test="username"]').send_keys('standard_user')
+    browser.find_element('xpath', '//input[@data-test="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@data-test="login-button"]').click()
+    time.sleep(2)
+    yield standard_auth
