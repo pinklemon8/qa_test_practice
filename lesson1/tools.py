05c753379a4de162b3e005d197152c6e9377487f
Aiuna San <158786728+pinklemon8@users.noreply.github.com>
2024-04-04 19:12:19 +0200

    modified .gitignore

--- lesson1/tools.py
+++ lesson1/tools.py
@@ -8,10 +8,10 @@ browser = webdriver.Chrome()
 
 # Фикстура для авторизации
 @pytest.fixture()
-def standard_auth():
+def login():
     browser.get(url)
     browser.find_element('xpath', '//input[@data-test="username"]').send_keys('standard_user')
     browser.find_element('xpath', '//input[@data-test="password"]').send_keys('secret_sauce')
     browser.find_element('xpath', '//input[@data-test="login-button"]').click()
     time.sleep(2)
-    yield standard_auth
+    yield login
