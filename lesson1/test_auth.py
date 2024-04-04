b7b476a6cb85a96ce78ab301e401830e5761fcdc
Aiuna San <158786728+pinklemon8@users.noreply.github.com>
2024-04-04 19:21:08 +0200

    added changes to test_auth.py added changes to .gitignore changed requirements.txt changed test_case_auth.md

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -1,17 +1,60 @@
-from selenium import webdriver
+from tools import *
 from selenium.webdriver.common.by import By
-import pytest
 
 
-def test_auth_positive():
-
-    browser = webdriver.Chrome()
-    browser.get('https://www.saucedemo.com/v1/')
-
-    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('problem_user')
-    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
-    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html', 'Error: url does not expected'
+# 1
+def test_standard_user(browser):
+    browser.get(url)
+    browser.find_element(By.ID, "user-name").send_keys('standard_user')
+    browser.find_element(By.ID, "password").send_keys('secret_sauce')
+    browser.find_element(By.ID, "login-button").click()
+    assert browser.current_url == catalog, f'Authorisation unsuccessful'
 
 
-    browser.quit()
+# 2
+def test_locked_out_user(browser):
+    try:
+        browser.get(url)
+        browser.find_element(By.ID, "user-name").send_keys('locked_out_user')
+        browser.find_element(By.ID, "password").send_keys('secret_sauce')
+        browser.find_element(By.ID, "login-button").click()
+        assert browser.current_url == catalog, 'Authorisation unsuccessful'
+    except AssertionError:
+        print(f'Epic sadface: Sorry, this user has been locked out.')
+
+    # 3
+
+
+def test_problem_user(browser):
+    browser.get(url)
+    browser.find_element(By.ID, "user-name").send_keys('problem_user')
+    browser.find_element(By.ID, "password").send_keys('secret_sauce')
+    browser.find_element(By.ID, "login-button").click()
+    assert browser.current_url == catalog, f'Authorisation unsuccessful'
+
+
+# 4
+def test_performance_glitch_user(browser):
+    browser.get(url)
+    browser.find_element(By.ID, "user-name").send_keys('performance_glitch_user')
+    browser.find_element(By.ID, "password").send_keys('secret_sauce')
+    browser.find_element(By.ID, "login-button").click()
+    assert browser.current_url == catalog, f'Authorisation unsuccessful'
+
+
+# 5
+def test_error_user(browser):
+    browser.get(url)
+    browser.find_element(By.ID, "user-name").send_keys('error_user')
+    browser.find_element(By.ID, "password").send_keys('secret_sauce')
+    browser.find_element(By.ID, "login-button").click()
+    assert browser.current_url == catalog, f'Authorisation unsuccessful'
+
+
+# 6
+def test_visual_user(browser):
+    browser.get(url)
+    browser.find_element(By.ID, "user-name").send_keys('visual_user')
+    browser.find_element(By.ID, "password").send_keys('secret_sauce')
+    browser.find_element(By.ID, "login-button").click()
+    assert browser.current_url == catalog, f'Authorisation unsuccessful'
--- lesson1/test_case_auth.md
+++ lesson1/test_case_auth.md
@@ -1,9 +1,52 @@
-[//]: # (Authorisation)
+Авторизация
+1). Авторизация, используя корректные данные (standard_user, secret_sauce)
+1. Открыть url: https://www.saucedemo.com
+2. Введите "standard_user" в поле Username
+3. Введите "secret_sauce" в поле Password
+4. Нажмите на зеленую кнопку "Login"
+5. Проверьте, что мы перешли на url : https://www.saucedemo.com/inventory.html
 
-[//]: # (I. Use data: 'standard_user, secret_sauce' for authorisation)
-1. open url: https://www.saucedemo.com/v1/
-2. enter login "standart_user"
-3. enter password "secret_sauce"
-4. tap button "LOGIN"
-5. check that opened url is: https://www.saucedemo.com/v1/inventory.html
-6. 
+2). Авторизация, используя некорректные данные (user, user)
+1. Открыть url: https://www.saucedemo.com
+2. Введите "user" в поле Username
+3. Введите "user"" в поле Password
+4. Нажмите на зеленую кнопку "Login"
+5. Проверьте, что мы перешли на url: https://www.saucedemo.com/inventory.html
+
+Корзина
+1). Добавление товара в корзину через каталог
+Авторизация:
+1. Открыть url https://www.saucedemo.com
+2. Ввести standard_user в поле Username
+3. Ввести secret_sauce в поле Password
+4. Нажмите на зеленую кнопку "Login"
+5. Проверьте, что мы перешли на url: https://www.saucedemo.com/inventory.html
+
+Добавление товара:
+1. Нажмите на кнопку Add to cart напротив выбранного товара
+2. Проверьте, что кнопка Add to cart переименовалась на Remove
+
+Удаление товара из корзины через корзину
+Авторизация:
+1. Открыть url https://www.saucedemo.com
+2. Ввести standard_user в поле Username
+3. Ввести secret_sauce в поле Password
+4. Нажмите на зеленую кнопку "Login"
+5. Проверьте, что мы перешли на url: https://www.saucedemo.com/inventory.html
+
+Удаление товара:
+1. Нажмите на значок корзины, чтобы перейти на url: https://www.saucedemo.com/cart.html 
+2. Нажмите на кнопку Remove, чтобы удалить товар из корзины
+3. Проверьте, что товар удален
+
+Добавление товара в корзину из карточки товара
+Авторизация:
+1. Открыть url https://www.saucedemo.com
+2. Ввести standard_user в поле Username
+3. Ввести secret_sauce в поле Password
+4. Нажмите на зеленую кнопку "Login"
+5. Проверьте, что мы перешли на url: https://www.saucedemo.com/inventory.html
+
+Добавление товара:
+1. 
+4.) Удаление товара из корзины через карточку товара
