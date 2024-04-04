6e88c5e95d446f2c32e78bb659e12bb1ca3cfbd7
Aiuna San <158786728+pinklemon8@users.noreply.github.com>
2024-04-04 19:18:44 +0200

    rename fixtures.py => tools.py

--- lesson1/tools.py
+++ lesson1/tools.py
@@ -1,17 +1,12 @@
 from selenium import webdriver
-import time
 import pytest
 
 url = 'https://www.saucedemo.com/'
-browser = webdriver.Chrome()
+catalog = 'https://www.saucedemo.com/inventory.html'
 
 
-# Фикстура для авторизации
-@pytest.fixture()
-def login():
-    browser.get(url)
-    browser.find_element('xpath', '//input[@data-test="username"]').send_keys('standard_user')
-    browser.find_element('xpath', '//input[@data-test="password"]').send_keys('secret_sauce')
-    browser.find_element('xpath', '//input[@data-test="login-button"]').click()
-    time.sleep(2)
-    yield login
+@pytest.fixture(scope="function")
+def browser():
+    driver = webdriver.Chrome()  # Инициализация браузера
+    yield driver
+    driver.quit()  # Завершение сеанса браузера после выполнения теста
